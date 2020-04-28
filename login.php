05f998d51c3df05cf2107e18ffa25fc198524d63
Sonyas1982 <62689103+Sonyas1982@users.noreply.github.com>
2020-04-28 11:19:27 +0300

    Add files via upload
    Kaikki toimii

--- login.php
+++ login.php
@@ -1,28 +1,43 @@
 
-<?php 
-session_start();
-require_once('db.php');
-// Kirjaudutaan tietokantaan
-$conn = db_connect();
-
+<?php
+// Haetaan tiedot lomakkeelta muuttujiin
 $user = $_POST["kayttaja"];
 $pass = $_POST["salasana"];
 
 
-//haetaan käyttäjätiedot 
-$sql = "select kayttaja, salasana FROM tunnukset WHERE kayttaja = '$user' AND salasana = '$pass'";
-$results = $conn->query($sql) or die( $conn->error ); 
- 
+// Otetaan yhteys tietokantaan
+$palvelin = "localhost";
+$kayttaja = "id13358397_keuda";
+$salasana = "Ohtuproto2020!";
+$tietokanta = "id13358397_user";
+
+$conn = mysqli_connect($palvelin, $kayttaja, $salasana, $tietokanta);
+
+if(mysqli_connect_error()){
+
+	die("Tietokantaan ei saatu yhteyttä");
+
+	}
+//haetaan käyttäjätiedot
+$sql = "select kayttaja, salasana FROM tunnukset WHERE kayttaja = '$user' AND salasana = MD5('$pass')";
+$results = $conn->query($sql) or die( $conn->error );
+
 if (!$results) {
-	echo "database is empty!"; 
+	echo "database is empty!";
 	exit();
 }
 
 if ($results->num_rows == 1) {  //Kirjautuminen onnistui
 
 
-	$_SESSION["kayttaja"] = $user
-	header("Location: muistipeli.html");
+	$_SESSION["kayttaja"] = $user;
+	?>
+
+	<script>
+	    window.location.href = "muistipeli.html";
+	</script>
+
+	<?php
 } else echo "Login fails! <br>";
 
 
---
+++ uusikayttaja.php
@@ -0,0 +1,27 @@
+<?php
+// Haetaan tiedot lomakkeelta muuttujiin
+$user = $_POST["kayttaja"];
+$pass = $_POST["salasana"];
+
+
+// Otetaan yhteys tietokantaan
+$palvelin = "localhost";
+$kayttaja = "id13358397_keuda";
+$salasana = "Ohtuproto2020!";
+$tietokanta = "id13358397_user";
+
+$link = mysqli_connect($palvelin, $kayttaja, $salasana, $tietokanta);
+
+if(mysqli_connect_error()){
+
+	die("Tietokantaan ei saatu yhteyttä");
+
+	}
+
+$kysely = "INSERT INTO tunnukset (kayttaja, salasana) VALUES ('$user', MD5('$pass'))";
+if (mysqli_query($link, $kysely)) {
+      echo "Uudet tiedostot paivitetty";
+} else {
+      echo "Virhe: " . $kysely . "<br>" . mysqli_error($link);
+}
+mysqli_close($link);
